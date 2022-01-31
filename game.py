5711f9bb80d6edcbeed96f8e8bec820e360b5660
jkp53 <80708001+jkp53@users.noreply.github.com>
2022-01-31 16:45:04 -0500

    playing around to try to automate testing

--- game.py
+++ game.py
@@ -12,54 +12,67 @@ import os
 
 player_name = os.getenv("PLAYER_NAME", default="Player One")
 
-#
-print("Welcome", player_name, "to my Rock-Paper-Scissors game...")
 
-# ASK FOR USER INPUT
+def determine_winner(user_choice, computer_choice):
+    """
+    Determines the winning choice between two valid choices from selectable options: "rock", "paper", or "scissors".
 
-user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ").casefold()
+    Returns the winning choice (e.g. "paper"), or None if there is a tie.
 
-# VALIDATE USER INPUTS
+    Example: determine_winner("rock", "paper")
+    """
+    # todo: write some Python here to determine the winner
 
-#this stores the valid choices in a list
-valid_choices = ['rock', 'paper', 'scissors']
-
-#this exits the program if the user doesn't enter one of the valid choices
-if user_choice in valid_choices:
-    print("YOU CHOSE:", user_choice)
-else:
-    print("Oops, incorrect entry. Please try again.")
-    exit()
+    return "todo"
 
 
-# COMPUTER CHOICE
-#this randomizes the computer's choice and assigns the choice to a variable
-import random
-computer_choice = random.choice(valid_choices)
-print("THE COMPUTER CHOSE:", computer_choice)
+if __name__ == "__main__":
+
+    print("Welcome", player_name, "to my Rock-Paper-Scissors game...")
+
+    # ASK FOR USER INPUT
+
+    user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ").casefold()
+
+    # VALIDATE USER INPUTS
+
+    #this stores the valid choices in a list
+    valid_choices = ['rock', 'paper', 'scissors']
+
+    #this exits the program if the user doesn't enter one of the valid choices
+    if user_choice in valid_choices:
+        print("YOU CHOSE:", user_choice)
+    else:
+        print("Oops, incorrect entry. Please try again.")
+        exit()
 
 
-
-# DETERMINE THE WINNER
-
-if user_choice == computer_choice:
-    print("The game ends in a tie.")
-elif ((user_choice == "rock") and (computer_choice == "scissors")):
-        print("You won! since", user_choice, "beats", computer_choice)
-elif ((user_choice == "paper") and (computer_choice == "rock")):
-        print("You won! since", user_choice, "beats", computer_choice)
-elif ((user_choice == "scissors") and (computer_choice == "paper")):
-        print("You won! since", user_choice, "beats", computer_choice)
-elif ((user_choice == "rock") and (computer_choice == "paper")):
-        print("Bummer, the computer won since", computer_choice, "beats", user_choice)
-elif ((user_choice == "scissors") and (computer_choice == "rock")):
-        print("Bummer, the computer won since", computer_choice, "beats", user_choice)
-elif ((user_choice == "paper") and (computer_choice == "scissors")):
-        print("Bummer, the computer won since", computer_choice, "beats", user_choice)
+    # COMPUTER CHOICE
+    #this randomizes the computer's choice and assigns the choice to a variable
+    import random
+    computer_choice = random.choice(valid_choices)
+    print("THE COMPUTER CHOSE:", computer_choice)
 
 
 
+    # DETERMINE THE WINNER
 
-# FINAL RESULTS
+    if user_choice == computer_choice:
+        print("The game ends in a tie.")
+    elif ((user_choice == "rock") and (computer_choice == "scissors")):
+            print("You won! since", user_choice, "beats", computer_choice)
+    elif ((user_choice == "paper") and (computer_choice == "rock")):
+            print("You won! since", user_choice, "beats", computer_choice)
+    elif ((user_choice == "scissors") and (computer_choice == "paper")):
+            print("You won! since", user_choice, "beats", computer_choice)
+    elif ((user_choice == "rock") and (computer_choice == "paper")):
+            print("Bummer, the computer won since", computer_choice, "beats", user_choice)
+    elif ((user_choice == "scissors") and (computer_choice == "rock")):
+            print("Bummer, the computer won since", computer_choice, "beats", user_choice)
+    elif ((user_choice == "paper") and (computer_choice == "scissors")):
+            print("Bummer, the computer won since", computer_choice, "beats", user_choice)
 
-print("Thanks for playing. Please play again!")
+
+    # FINAL RESULTS
+
+    print("Thanks for playing. Please play again!")
